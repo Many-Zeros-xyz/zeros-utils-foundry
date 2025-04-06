91e46201cae7f59ef62928653ad63c9f30ead532
philogy <philippe@dumo.net>
2025-04-06 14:05:31 +0100

    ✨ Add contracts

---
+++ src/IManyZerosMarket.sol
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+IManyZerosMarket constant MANY_ZEROS_MARKET = IManyZerosMarket(0x000000000000b361194cfe6312EE3210d53C15AA);
+
+/// @author philogy <https://github.com/philogy>
+interface IManyZerosMarket {
+    error AccountBalanceOverflow();
+    error AlreadyInitialized();
+    error AlreadyMinted();
+    error BalanceQueryForZeroAddress();
+    error DeploymentFailed();
+    error DomainSeparatorsInvalidated();
+    error InsufficientValue();
+    error InvalidSignature();
+    error NoRenderer();
+    error NonceAlreadyInvalidated();
+    error NotAuthorizedBuyer();
+    error NotAuthorizedClaimer();
+    error NotOwnerNorApproved();
+    error PastDeadline();
+
+    error TokenAlreadyExists();
+    error TokenDoesNotExist();
+    error TransferFromIncorrectOwner();
+    error TransferToNonERC721ReceiverImplementer();
+    error TransferToZeroAddress();
+    error Unauthorized();
+
+    event Approval(address indexed owner, address indexed account, uint256 indexed id);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool isApproved);
+    event FeeSet(uint16 fee);
+    event OwnershipHandoverCanceled(address indexed pendingOwner);
+    event OwnershipHandoverRequested(address indexed pendingOwner);
+    event OwnershipTransferred(address indexed oldOwner, address indexed newOwner);
+    event RendererSet(address indexed renderer);
+    event Transfer(address indexed from, address indexed to, uint256 indexed id);
+
+    function DEPLOY_PROXY_INITHASH() external view returns (bytes32);
+
+    ////////////////////////////////////////////////////////////////
+    //                           ERC712                           //
+    ////////////////////////////////////////////////////////////////
+
+    function CROSS_CHAIN_DOMAIN_SEPARATOR() external view returns (bytes32);
+    function FULL_DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    function getNonceIsSet(address user, uint256 nonce) external view returns (bool);
+    function invalidateNonce(uint256 nonce) external;
+
+    ////////////////////////////////////////////////////////////////
+    //                        VANITY TOKEN                        //
+    ////////////////////////////////////////////////////////////////
+
+    function computeAddress(bytes32 salt, uint8 nonce) external view returns (address vanity);
+    function feeBps() external view returns (uint16);
+    function calculateBuyCost(uint256 sellerPrice) external view returns (uint256);
+
+    function mint(address to, uint256 id, uint8 nonce) external;
+    function mintAndBuyWithSig(
+        address to,
+        uint256 id,
+        uint8 saltNonce,
+        address beneficiary,
+        uint256 sellerPrice,
+        address buyer,
+        uint256 nonce,
+        uint256 deadline,
+        bytes memory signature
+    ) external payable;
+    function claimGivenUpWithSig(
+        address to,
+        uint256 id,
+        uint8 nonce,
+        address claimer,
+        uint256 deadline,
+        bytes memory signature
+    ) external;
+
+    function addressOf(uint256 id) external view returns (address vanity);
+    function deploy(uint256 id, bytes memory initcode) external payable returns (address deployed);
+    function getTokenData(uint256 id) external view returns (bool minted, uint8 nonce);
+
+    ////////////////////////////////////////////////////////////////
+    //                          METADATA                          //
+    ////////////////////////////////////////////////////////////////
+
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+
+    function name() external pure returns (string memory);
+    function symbol() external pure returns (string memory);
+    function tokenURI(uint256 id) external view returns (string memory);
+    function contractURI() external view returns (string memory);
+
+    function owner() external view returns (address result);
+    function royaltyInfo(uint256 tokenId, uint256 salePrice)
+        external
+        view
+        returns (address receiver, uint256 royaltyAmount);
+
+    ////////////////////////////////////////////////////////////////
+    //                           ERC721                           //
+    ////////////////////////////////////////////////////////////////
+
+    function balanceOf(address owner) external view returns (uint256 result);
+    function ownerOf(uint256 id) external view returns (address result);
+    function getApproved(uint256 id) external view returns (address result);
+    function isApprovedForAll(address owner, address operator) external view returns (bool result);
+
+    function setApprovalForAll(address operator, bool isApproved) external;
+    function approve(address account, uint256 id) external;
+    function permitForAll(address owner, address operator, uint256 nonce, uint256 deadline, bytes memory signature)
+        external;
+
+    function transferFrom(address from, address to, uint256 id) external;
+    function safeTransferFrom(address from, address to, uint256 id) external;
+    function safeTransferFrom(address from, address to, uint256 id, bytes memory data) external;
+}
---
+++ src/ManyZerosLib.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IManyZerosMarket, MANY_ZEROS_MARKET} from "./IManyZerosMarket.sol";
+
+struct GiveUpEverywhere {
+    uint256 id;
+    uint8 nonce;
+    address claimer;
+    uint256 deadline;
+}
+
+struct MintAndSell {
+    uint256 id;
+    uint8 saltNonce;
+    uint256 price;
+    address beneficiary;
+    address buyer;
+    uint256 nonce;
+    uint256 deadline;
+}
+
+using ManyZerosLib for GiveUpEverywhere global;
+using ManyZerosLib for MintAndSell global;
+
+/// @author philogy <https://github.com/philogy>
+library ManyZerosLib {
+    using ManyZerosLib for IManyZerosMarket;
+
+    /// @dev keccak256("GiveUpEverywhere(uint256 id,uint8 nonce,address claimer,uint256 deadline)")
+    bytes32 internal constant GIVE_UP_EVERWHERE_TYPEHASH =
+        0x3d27e7c5806b97e91e45fdf6c7d4b7e7b8aa85f091a2fa2999a87ea3bafc7ca6;
+
+    /// @dev keccak256("MintAndSell(uint256 id,uint8 saltNonce,uint256 price,address beneficiary,address buyer,uint256 nonce,uint256 deadline)")
+    bytes32 internal constant MINT_AND_SELL_TYPEHASH =
+        0xd7297ff8cc8b0abf17e5590f0b14e1c876bb758ce21d250e9d39e37f61d77f5e;
+
+    /// @dev Computes the ERC-712 struct hash of `self`.
+    function structHash(GiveUpEverywhere memory self) internal pure returns (bytes32) {
+        return keccak256(abi.encode(GIVE_UP_EVERWHERE_TYPEHASH, self.id, self.nonce, self.claimer, self.deadline));
+    }
+
+    /// @dev Computes the ERC-712 struct hash of `self`.
+    function structHash(MintAndSell memory self) internal pure returns (bytes32) {
+        return keccak256(
+            abi.encode(
+                MINT_AND_SELL_TYPEHASH,
+                self.id,
+                self.saltNonce,
+                self.price,
+                self.beneficiary,
+                self.buyer,
+                self.nonce,
+                self.deadline
+            )
+        );
+    }
+
+    function digestHash(IManyZerosMarket market, GiveUpEverywhere memory giveUp) internal view returns (bytes32) {
+        bytes32 crossChainDomain = market.CROSS_CHAIN_DOMAIN_SEPARATOR();
+        return keccak256(abi.encodePacked(bytes2(0x1901), crossChainDomain, giveUp.structHash()));
+    }
+
+    function digestHash(IManyZerosMarket market, MintAndSell memory mintAndSell) internal view returns (bytes32) {
+        bytes32 crossChainDomain = market.FULL_DOMAIN_SEPARATOR();
+        return keccak256(abi.encodePacked(bytes2(0x1901), crossChainDomain, mintAndSell.structHash()));
+    }
+
+    function default_digestHash(GiveUpEverywhere memory giveUp) internal view returns (bytes32) {
+        return MANY_ZEROS_MARKET.digestHash(giveUp);
+    }
+
+    function default_digestHash(MintAndSell memory mintAndSell) internal view returns (bytes32) {
+        return MANY_ZEROS_MARKET.digestHash(mintAndSell);
+    }
+
+    /// @dev Create an ID owned by `owner` with `ext` as the lower bits.
+    function ownerPlusExtensionToId(address owner, uint96 ext) internal pure returns (uint256 id) {
+        id = (uint256(uint160(owner)) << 96) | ext;
+    }
+}
